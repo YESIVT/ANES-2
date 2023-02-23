2820c73320a3771e301f57e0bac26b6bc0c4e187
YESIVT <yesivt@hotmail.com>
2023-02-23 13:30:17 -0600

    Tarea 03

---
+++ Tarea_03_Yesenia_Villarreal_Torres.R
@@ -0,0 +1,53 @@
+#Yesenia Villarreal Torres
+#Tarea_03
+#23/02/2023
+
+
+# Problema 1 --------------------------------------------------------------
+
+
+xi <- c(6,4,1,3)
+xi
+yi <- c(1,3,4,2)
+yi
+
+sum(xi)
+sum(xi, yi)
+prod(xi)
+prod(xi, yi)
+
+prod((xi^2)*(yi^0.5))
+
+
+# Problema 2 --------------------------------------------------------------
+
+Grupo_A <- c(80, 90, 90, 100)
+Grupo_B <- c(60, 65, 65, 70, 70, 70, 75, 75, 80, 80, 80, 80, 80, 85, 100)
+
+#Grupo A, con media mayor
+
+mean(Grupo_A)
+mean(Grupo_B)     
+# si coincidió, los diámetros son mayores y menos número de individuos
+
+
+# Problema 3 --------------------------------------------------------------
+#José quiere tener un promedio de 80 en sus 4 exámenes. Cada examen se califica en una escalade 0 a 100. Sus primeros tres exámenes son: 87, 72, 85. ¿Qué necesita José para calificar en el 4to. examen para tener una media de 80 en los 4 exámenes?
+
+calif <- c(87,72,85)
+sum(calif)
+mean(calif)
+(4*80)-(sum(calif))
+# calificación al menos de 76, para que el mean sea 80
+
+
+# Problema 4 --------------------------------------------------------------
+
+#El comité escolar de una pequeña ciudad quiere determinar el número promedio de niños por hogar en su ciudad. Hay 50 hogares en la ciudad. Ellos dividen el total número de niños en la ciudad por 50 y determine que el número promedio de niños por hogar es 2.2. ¿Cuál de lo siguientes enunciados debe ser verdad?
+2.2*50
+#La respuesta es b, solo se multiplica el promedio por el número total de hogares
+
+
+# Problema 5 --------------------------------------------------------------
+
+
