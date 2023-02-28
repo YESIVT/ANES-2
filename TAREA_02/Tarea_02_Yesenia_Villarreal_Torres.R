2664bac2a4b3279a62e8ab023d31051f77c712c8
YESIVT <yesivt@hotmail.com>
2023-02-28 14:44:57 -0600

    Tarea 4

---
+++ TAREA_02/Tarea_02_Yesenia_Villarreal_Torres.R
@@ -0,0 +1,13 @@
+#Yesenia Villarreal Torres
+#Matrícula 1109559
+#Fecha 28 de febrero 2023
+#Tarea 4
+
+#Histogramas
+#Problema 1
+#Considere el siguiente conjunto de datos x2 con 1000 números al azar.
+
+set.seed(9875)
+size <- 1000
+x2 <- round(runif(n = size, min = 0, max = 10), 2)
+x2
