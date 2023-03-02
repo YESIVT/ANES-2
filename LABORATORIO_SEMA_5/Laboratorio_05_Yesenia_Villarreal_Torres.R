c55e6fcc71a49cd5de6d104a583be683006b1c0a
YESIVT <yesivt@hotmail.com>
2023-03-02 12:48:57 -0600

    Laboratorio

--- LABORATORIO_SEMA_5/Laboratorio_05_Yesenia_Villarreal_Torres.R
+++ LABORATORIO_SEMA_5/Laboratorio_05_Yesenia_Villarreal_Torres.R
@@ -16,6 +16,13 @@ plot(anscombe$x4, anscombe$y4, pch = 20)
 par(op)
 
 #Coeficiente de correlación
+
+cor.test(anscombe$x1, anscombe$y1)
+cor.test(anscombe$x2, anscombe$y2)
+cor.test(anscombe$x3, anscombe$y3)
+cor.test(anscombe$x4, anscombe$y4)
+
+
 #Determinar para cada par de datos los coeficientes de correlación r.
 #¿Alguna sorpresa? Como puedes ver, los cuatro pares de las variables xy tienen básicamente la misma correlación de 0.816. Pero no todos tienen diagramas de dispersión en los que los puntos se agrupan alrededor de una línea.
 #El mensaje para llevar a casa es que el coeficiente de correlación puede ser engañoso en presencia de valores atípicos o asociación no lineal. Debido a esto, siempre es importante revisar los datos con un gráfico de dispersión.
