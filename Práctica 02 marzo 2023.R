a504d59956aa2003f0c3e5f64f3908f1cabfbdfc
YESIVT <yesivt@hotmail.com>
2023-03-29 14:41:59 -0600

    kj

--- Práctica 02 marzo 2023.R
+++ Práctica 02 marzo 2023.R
@@ -22,11 +22,11 @@ boxplot(conjunto$Altura ~ conjunto$Clase)
 # INVESTIGAR  conjunto2 <- with(conjunto, reorder(conjunto$Altura, conjunto$Clase, median , na.rm=T))
 
 cor.test(conjunto$Diametro, conjunto$Altura)
-#Aunque la correlación es baja la correlación es significativa, valor de t es menor a 0.05, tiene la tendencia que aumenta x y aumenta y
+#Aunque la correlación es baja la correlación es significativa, valor de p es menor a 0.05, tiene la tendencia que aumenta x y aumenta y
 
 plot(conjunto$Diametro, conjunto$Altura, pch = 19)
 
 # importante para las pruebas el valor de correlación y el valor de P y los grados de libertar 98ª de libertad siempre es N-2 para las correlaciones
-
+#queremos determinar si el diámetro es un buen predictor de la altura, tomar un grupo de arboles y predecir como está el comportamiento 
 
      
